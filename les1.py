43ffeaf44f5736eadc72df21d4381b2dd297c9f4
PascalPetri <9018608@student.zadkine.nl>
2026-01-29 15:26:29 +0100

    les1 Python Klassen & objecten
    versie 1.0

---
+++ les1.py
@@ -0,0 +1,11 @@
+# Een class is een blauwdruk
+# Een object is een concrete instantie van die class
+
+class Student:
+    pass
+
+student1 = Student()
+student2 = Student()
+
+print(student1)
+print(student2)
